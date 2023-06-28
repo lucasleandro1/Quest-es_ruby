31a9f074b23a92b8e962ce304034bf4372fddd79
lucasleandro1 <lukkassoliveira2215@gmail.com>
2023-06-28 19:10:51 -0300

    add

--- heranca.rb
+++ heranca.rb
@@ -1,17 +1,42 @@
-class Franquia
-    def descricao
-        "franquia!"
+# class Franquia
+#     def descricao
+#         "franquia!"
+#     end
+# end
+
+# class Franqueado < Franquia
+#     def descricao
+#         puts super
+#         "franquiado!"
+#     end
+# end
+
+# f = Franquia.new
+# puts f.descricao
+# puts "============"
+# ff = Franqueado.new
+# puts ff.descricao
+
+class Conta
+    attr_reader :numero, :saldo
+    def initialize(numero,saldo = 0)
+        @numero = numero
+        @saldo = saldo
     end
 end
 
-class Franqueado < Franquia
-    def descricao
-        "franquiado!"
+class ContaEspecial < Conta
+    attr_reader :limite_especial
+    def initialize(numero, saldo, limite_especial)
+        super(numero,saldo)
+        @limite_especial = limite_especial
     end
 end
-
-f = Franquia.new
-puts f.descricao
-puts "============"
-ff = Franqueado.new
-puts ff.descricao
+c = Conta.new("001",100.00)
+puts c.numero
+puts c.saldo
+puts "---------"
+ce = ContaEspecial.new("002", 200.00, 1000.00)
+puts ce.numero
+puts ce.saldo
+puts ce.limite_especial
