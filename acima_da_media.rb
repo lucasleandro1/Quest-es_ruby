1b5bb4d07f86893a56e0e46ce1dc0bbe16a7ae80
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-02-06 11:57:30 -0300

    Add question

---
+++ acima_da_media.rb
@@ -0,0 +1,19 @@
+def percentual_media(notas)
+    media = notas.sum / notas.length.to_f
+    percentual = notas.count {|notas| notas > media}
+    result = (percentual * 100)/ notas.length.to_f
+    return format('%.3f', result)
+end
+puts "casos de teste: "
+casos = gets.to_i
+casos.times do 
+puts"alunos:"
+alunos = gets.chomp.to_i
+
+puts"notas: "
+notas = gets.split.map(&:to_i)
+
+m = percentual_media(notas)
+puts "#{m}%"
+
+end
