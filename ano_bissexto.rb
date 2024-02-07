aa432d5468901d62f87b3502314c2c0fc0459c5c
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-02-07 11:19:46 -0300

    Add question ano_bissexto

--- ano_bissexto.rb
+++ ano_bissexto.rb
@@ -1,5 +1,3 @@
-
-
 def AnoBissexto(ano)
     (ano % 4 == 0 and ano % 100 !=0 ) or (ano % 400 ==0) 
 end
@@ -15,19 +13,3 @@ while (ano = gets.chomp.to_i)
     a = AnoFestival(ano)
     puts "#{a}"
 end
-# def ano_bissexto(ano)
-#     (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)
-#   end
-  
-#   def propriedades_do_ano(ano)
-#     puts "This is a leap year." if ano_bissexto(ano)
-#     puts "This is huluculu festival year." if ano % 15 == 0
-#     puts "This is bulukulu festival year." if ano_bissexto(ano) && ano % 55 == 0
-#     puts "This is an ordinary year." unless ano_bissexto(ano) || (ano % 15 == 0 && ano % 55 != 0)
-#   end
-  
-#   # Leitura da entrada até EOF
-#   while (ano = gets&.to_i)
-#     propriedades_do_ano(ano)
-#     puts ""  # Linha em branco para separar os casos de teste
-#   end
